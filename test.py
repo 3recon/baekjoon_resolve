d7f42d23e016ecf5464742e17c8acb336dbf3d2a
recon3 <jch200325@naver.com>
2026-01-12 00:44:36 +0900

    Tier Up G5->G4

--- test.py
+++ test.py
@@ -1,23 +1,32 @@
-#1697 나중에 반드시 다시 풀기!!
-
 from collections import deque
+import sys
+input=sys.stdin.readline
 
-n,k=map(int,input().split())
 
-lst=[0]*100000
+def bfs(s1,s2):
+    dq=deque([[s1,s2]])
 
-def bfs(start):
-    dq=deque()
-    dq.append(start)
-    
     while dq:
         cur=dq.popleft()
-        if cur==k:
-            return lst[cur]
+        x=cur[0]
+        y=cur[1]
+        moves=[(x+2,y+1),(x+2,y-1),(x-2,y+1),(x-2,y-1),(x+1,y+2),(x+1,y-2),(x-1,y+2),(x-1,y-2)]
+        
+        if x==end1 and y==end2:
+            return matrix[x][y]
+        for move1,move2 in moves:
+            if 0<=move1<leng and 0<=move2<leng and matrix[move1][move2]==0:
+                matrix[move1][move2]=matrix[x][y]+1
+                dq.append([move1,move2])
 
-        for i in (cur-1,cur+1,cur*2):
-            if 0<=i<=100000 and lst[i]==0:
-                lst[i]=lst[cur]+1
-                dq.append(i)
 
-print(bfs(n))
+
+n=int(input())
+for i in range(n):
+    leng=int(input())
+    matrix=[[0 for j in range(leng)]for h in range(leng)]
+
+    start1,start2=map(int,input().split())
+    end1,end2=map(int,input().split())
+    print(bfs(start1,start2))
+
